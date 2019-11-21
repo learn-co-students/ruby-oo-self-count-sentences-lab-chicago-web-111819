06b7415ccfcf2435e11c26022cbec3e4141d145d
dut-a <d.dut@live.com>
2019-11-21 16:26:53 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,35 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    if self.chars.last == "."
+      return true
+    end
+    return false
   end
 
   def question?
-
+    if self.chars.last == "?"
+      return true
+    end
+    return false
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    end
+    return false
   end
 
   def count_sentences
+    # split the string based on given punctuation marks: '.', '!', '?'
+    str = self.split(/[.!?]/) # regex
+  
+    # remove empty sentences
+    str = str.reject {|s| s.empty? }
 
+    # return the sentence count
+    return str.size
   end
 end
