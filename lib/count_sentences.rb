ec3853a5f0cd87563f758f54b1c30807b22e4338
github username <github email address>
2019-11-22 12:43:56 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1] == "."
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    str = self.split(/[.?!]/) #split utilizing regex affecting all punctuation to end sentences
+    str = str.reject {|s| s.empty? } #gets rid of empty arrays caused by double punctuation
+    return str.length
   end
 end
